1cd2ba692a17a7191fccfcf8426c8416de40a8bc
david.gonzalez <david.gonzalez@CDS1UNIVO30>
2019-08-19 11:48:32 -0600

    h

--- Estudiante/Estudiante/Estudiante/Service/caseProperty.cs
+++ Estudiante/Estudiante/Estudiante/Service/caseProperty.cs
@@ -14,7 +14,7 @@ namespace Estudiante.Service
 
             protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
             {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
